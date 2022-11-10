9009bf593f78278a0fff3e93985a21e112ba5142
nikitabolkar123 <bolkarnikita2001@gmail.com>
2022-11-10 02:08:32 -0800

    Added Solution

---
+++ Generics/ExtendMaxMethods.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generics
+{
+    public class MaximumNumber<T> where T : IComparable<T>
+    {
+        public T[] inputArray;
+
+        public MaximumNumber(T[] inputArray)
+        {
+            this.inputArray = inputArray;
+        }
+        public T[] Sort(T[] inputArray)
+        {
+            Array.Sort(inputArray);
+            return inputArray;
+        }
+
+        public T MaxValue(params T[] inputArray)
+        {
+            var sortedValue = Sort(inputArray);
+            return sortedValue[^1];
+
+        }
+        public T MaxMethod()
+        {
+            var max = MaxValue(this.inputArray);
+            return max;
+        }
+        public void PrintMaxValue()
+        {
+            var max = MaxValue(this.inputArray);
+            Console.WriteLine($"Maximum value :{max}");
+        }
+    }
+}
--- Generics/Program.cs
+++ Generics/Program.cs
@@ -2,16 +2,13 @@
 
 namespace Generics
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            int output = MaximumNumber<int>.GetMaximum(15, 25, 35);
-            Console.WriteLine($"max={output}");
-            float output1 = MaximumNumber<float>.GetMaximum(15.5f, 25.5f, 35.5f);
-            Console.WriteLine($"max={output1}");
-            string output2 = MaximumNumber<string>.GetMaximum("abc", "pqr", "lmn");
-            Console.WriteLine($"max={output2}");
+            int[] array = { 11, 22, 33, 66, 77,88 };
+            MaximumNumber<int> generic = new MaximumNumber<int>(array);
+            generic.PrintMaxValue();
         }
 
     }
