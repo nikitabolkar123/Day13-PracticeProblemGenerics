b938fd24c67e28083250b9bc126100a2756439ff
nikitabolkar123 <bolkarnikita2001@gmail.com>
2022-11-10 01:06:31 -0800

    Added Solution

---
+++ Generics/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Generics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("maximum Number is:");
+            int output = MaximumIntNumber.MaximumIntNum(15, 25, 35);
+            Console.WriteLine(output);
+        }
+    }
+}
