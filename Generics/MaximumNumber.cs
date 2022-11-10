b29c39e45373e05e14802b7c7d861489e9f900b4
nikitabolkar123 <bolkarnikita2001@gmail.com>
2022-11-10 01:15:33 -0800

    Added Solution

---
+++ Generics/MaximumNumber.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generics
+{
+    public class MaximumNumber
+    {
+        public static int MaximumNum(int firstValue, int secondValue, int thirdValue)
+        {
+            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
+
+            {
+                return firstValue;
+            }
+            if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0)
+            {
+                return secondValue;
+            }
+            if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) > 0)
+            {
+                return thirdValue;
+            }
+            return firstValue;
+        }
+
+
+        public static Double MaximumNum(double firstValue, double secondValue, double thirdValue)
+        {
+            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
+
+            {
+                return firstValue;
+            }
+            if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0)
+            {
+                return secondValue;
+            }
+            if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) > 0)
+            {
+                return thirdValue;
+            }
+            return firstValue;
+        }
+
+        public static string MaximumNum(string firstValue, string secondValue, string thirdValue)
+        {
+            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
+
+            {
+                return firstValue;
+            }
+            if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0)
+            {
+                return secondValue;
+            }
+            if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) > 0)
+            {
+                return thirdValue;
+            }
+            return firstValue;
+        }
+
+    }
+}
--- Generics/Program.cs
+++ Generics/Program.cs
@@ -6,9 +6,19 @@ namespace Generics
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("maximum Number is:");
-            int output = MaximumIntNumber.MaximumIntNum(15, 25, 35);
+
+            Console.WriteLine("Maximum Interger Number is");
+            int output = MaximumNumber.MaximumNum(15, 25, 35);
             Console.WriteLine(output);
+
+            Console.WriteLine("Maximum Double Number is");
+            double output1 = MaximumNumber.MaximumNum(15.5, 25.5, 35.5);
+            Console.WriteLine(output1);
+
+            Console.WriteLine("Maximum String Number is");
+            string output2 = MaximumNumber.MaximumNum("abc", "xyz", "pqr");
+            Console.WriteLine(output2);
         }
+
     }
 }
